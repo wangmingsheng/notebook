fa607ad1eea39a4e5f18c4f0a0c22acf016c0236
wangmingsheng <1173816383@qq.com>
2017-05-15 21:12:07 +0800

    ...

--- frame/react/react project/entry/main.js
+++
@@ -1,6 +0,0 @@
-var ReactDOM = require('react-dom');
-var Index = require('../application/view/Index');
-var React = require('react')
-
-
-ReactDOM.render(React.createElement(Index), document.getElementById('bodyer'));
