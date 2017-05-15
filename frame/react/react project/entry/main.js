1b584b7e2f0e5256ceffcb0f2bacbb0cf72d7fc2
wangmingsheng <1173816383@qq.com>
2017-05-15 21:10:42 +0800

    ....

---
+++ frame/react/react project/entry/main.js
@@ -0,0 +1,6 @@
+var ReactDOM = require('react-dom');
+var Index = require('../application/view/Index');
+var React = require('react')
+
+
+ReactDOM.render(React.createElement(Index), document.getElementById('bodyer'));
